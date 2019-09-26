97680adae819ef9d05a919fb1a8fdd380c9d7a5c
esim7 <makishev@yahoo.com>
2019-09-26 16:27:32 +0600

    Добавлены классы

---
+++ JsonPract/RootObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonPract
+{
+    public class RootObject
+    {
+        public int ResultCount { get; set; }
+        public Result[] Results { get; set; }
+    }
+}
